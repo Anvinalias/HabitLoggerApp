68a62c9867cdde0771c74f98d62eabb767284047
Anvinalias <anvinkaippillil123@gmail.com>
2024-08-21 12:27:45 +0530

    Add menu option in a do while loop

--- HabitLogger.Anvinalias/HabitLogger.Anvinalias/Program.cs
+++ HabitLogger.Anvinalias/HabitLogger.Anvinalias/Program.cs
@@ -1,10 +1,23 @@
-﻿namespace HabitLogger.Anvinalias
-{
+﻿namespace HabitLogger.Anvinalias;
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string? userChoice;
+
+            do
+            {
+                Console.WriteLine("MAIN MENU");
+                Console.WriteLine("\n\nWhat would you like to do?");
+                Console.WriteLine("\nType '0' to Close Application.");
+                Console.WriteLine("Type '1' to View All Record.");
+                Console.WriteLine("Type '2' to Insert Record.");
+                Console.WriteLine("Type '3' to Update Record.");
+                Console.WriteLine("Type '4' to Delete Record.");
+
+                Console.WriteLine("\nEnter your choice: ");
+                userChoice = Console.ReadLine();
+
+            } while (userChoice != "0");
         }
     }
-}
