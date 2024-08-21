0269105d98218a095d59653c7d8b5628634bafda
Anvinalias <anvinkaippillil123@gmail.com>
2024-08-21 12:24:40 +0530

    Create a new project and add gitignore file

---
+++ HabitLogger.Anvinalias/HabitLogger.Anvinalias/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace HabitLogger.Anvinalias
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
