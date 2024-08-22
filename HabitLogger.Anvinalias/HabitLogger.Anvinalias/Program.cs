c09ad36468510f32a453ec646690739a6e8ad6d7
Anvinalias <anvinkaippillil123@gmail.com>
2024-08-23 00:32:36 +0530

    Add InsertRecord method and get methods for getting values for its variables

--- HabitLogger.Anvinalias/HabitLogger.Anvinalias/Program.cs
+++ HabitLogger.Anvinalias/HabitLogger.Anvinalias/Program.cs
@@ -51,18 +51,18 @@ internal class Program
             case "1":
                 DisplayAllRecord();
                 break;
-            //case "2":
-            //    InsertRecord();
-            //    break;
-            //case "3":
-            //    UpdateRecord();
-            //    break;
-            //case "4":
-            //    DeleteRecord();
-            //    break;
-            //default:
-            //    Console.WriteLine("Invalid Choice");
-            //    break;
+            case "2":
+                InsertRecord();
+                break;
+                //case "3":
+                //    UpdateRecord();
+                //    break;
+                //case "4":
+                //    DeleteRecord();
+                //    break;
+                //default:
+                //    Console.WriteLine("Invalid Choice");
+                //    break;
         }
 
          void DisplayAllRecord()
@@ -88,5 +88,42 @@ internal class Program
                 connection.Close();
             }
         }
+
+        void InsertRecord()
+        {
+            string logDate = GetLogDate();
+            int logQuantity = GetLogQuantity();
+
+            string insertQuery =
+                $"INSERT INTO habitLog (Date, Quantity) VALUES ('{logDate}',{logQuantity})";
+
+            connection.Open();
+            command = new SQLiteCommand(insertQuery, connection);
+            command.ExecuteNonQuery();
+            connection.Close();
+
+            Console.WriteLine("Data Inserted");
+        }
+
+        string GetLogDate()
+        {
+            string date;
+
+            Console.WriteLine("Enter the date (dd-mm-yy) of habit");
+            date = Console.ReadLine();
+
+            return date;
+        }
+
+        int GetLogQuantity()
+        {
+            int quantity;
+
+            Console.WriteLine("Enter the quantity of water to be logged");
+            quantity = Convert.ToInt32(Console.ReadLine());
+
+            return quantity;
+        }
+
     }
 }
