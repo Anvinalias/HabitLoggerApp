0a16adc8e704e4b49a0c226908682943911706e8
Anvinalias <anvinkaippillil123@gmail.com>
2024-08-23 00:13:56 +0530

    Create db and and table

--- HabitLogger.Anvinalias/HabitLogger.Anvinalias/Program.cs
+++ HabitLogger.Anvinalias/HabitLogger.Anvinalias/Program.cs
@@ -1,23 +1,49 @@
-﻿namespace HabitLogger.Anvinalias;
-    internal class Program
+﻿using System.Data.SQLite;
+
+namespace HabitLogger.Anvinalias;
+
+internal class Program
+{
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        string? userChoice;
+
+        SQLiteConnection connection;
+        SQLiteCommand command;
+        SQLiteDataReader reader;
+
+        string connectionString = @$"Data Source= HabitTracker.db";
+            
+
+        try
         {
-            string? userChoice;
-
-            do
+            using (connection = new SQLiteConnection(connectionString))
             {
-                Console.WriteLine("MAIN MENU");
-                Console.WriteLine("\n\nWhat would you like to do?");
-                Console.WriteLine("\nType '0' to Close Application.");
-                Console.WriteLine("Type '1' to View All Record.");
-                Console.WriteLine("Type '2' to Insert Record.");
-                Console.WriteLine("Type '3' to Update Record.");
-                Console.WriteLine("Type '4' to Delete Record.");
-
-                Console.WriteLine("\nEnter your choice: ");
-                userChoice = Console.ReadLine();
-
-            } while (userChoice != "0");
+                connection.Open();
+                command = connection.CreateCommand();
+                command.CommandText = "CREATE TABLE IF NOT EXISTS habitLog (Id INTEGER PRIMARY KEY AUTOINCREMENT, Date TEXT, Quantity INTEGER)"; ;
+                command.ExecuteNonQuery();
+                connection.Close();
+            }
         }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.ToString());
+        }
+
+        do
+        {
+            Console.WriteLine("MAIN MENU");
+            Console.WriteLine("\n\nWhat would you like to do?");
+            Console.WriteLine("\nType '0' to Close Application.");
+            Console.WriteLine("Type '1' to View All Record.");
+            Console.WriteLine("Type '2' to Insert Record.");
+            Console.WriteLine("Type '3' to Update Record.");
+            Console.WriteLine("Type '4' to Delete Record.");
+
+            Console.WriteLine("\nEnter your choice: ");
+            userChoice = Console.ReadLine();
+        
+        } while (userChoice != "0");
     }
+}
